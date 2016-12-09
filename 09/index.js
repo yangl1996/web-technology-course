5a51c668ff36b85341f65b3dcaec9a09c52a70fa
Lei Yang <yltt1234512@gmail.com>
2016-12-09 12:43:54 +0800

    add user

--- 09/index.js
+++ 09/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
-
+var user_db = require('./user-db.json');
+var user_list = user_db['list'];
+var user_size = user_db['size'];
 /* Application Configurations */
 
 app.use('/static', express.static(__dirname + '/static'));  // static files
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.set('views', './views');                    // template directory
 app.set('view engine', 'pug');                  // template engine
@@ -24,6 +30,27 @@ app.get('/survey', function(req, res) {
 app.get('/results', function(req, res) {
     res.render('results');
 });
+
+app.post('/survey', function(req, res) {
+    var user_name = req.body['name'];
+    var user_age = req.body['age'];
+    var user_email = req.body['email'];
+    var user_gender = 'N/A';
+    if ("gender" in req.body) {
+        var user_gender = req.body['gender'];
+    }
+    var added_entry = {'id': ++user_size, 'name': user_name, 'age': user_age, 'gender': user_gender, 'email': user_email};
+    user_list.push(added_entry);
+    var updated_db_str = JSON.stringify({'list': user_list, 'size': user_size}, null, 4);
+    var fs = require('fs');
+    fs.writeFile("./user-db.json", updated_db_str, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    });
+    res.redirect('/results');
+});
+
 
 app.listen(9096, function() {
     console.log('Server started.');
